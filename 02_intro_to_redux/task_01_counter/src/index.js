1c6f1a5e6df9b61181d1f3e81cd7f10d9092db9d
apache0505 <jiayulutsk@ukr.net>
2022-12-16 13:41:16 +0200

    sa

--- 02_intro_to_redux/task_01_counter/src/index.js
+++ 02_intro_to_redux/task_01_counter/src/index.js
@@ -25,6 +25,7 @@ const counterReducer = (state = 0, action) => {
   }
 };
 
-// const store = createStore(counterReducer);
+const store = createStore(counterReducer);
+
 // store.dispatch(increment());
 // store.dispatch(decrement());
