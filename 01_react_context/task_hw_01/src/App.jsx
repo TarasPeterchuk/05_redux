09f228f07129709648baa0609d113136442a99fc
apache0505 <jiayulutsk@ukr.net>
2022-12-16 11:37:50 +0200

    sa

--- 01_react_context/task_hw_01/src/App.jsx
+++ 01_react_context/task_hw_01/src/App.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import ThemedButton from './ThemedButton';
-import { themes, ThemeContext } from './themes-context';
+import Header from './Header';
+import { userData, UserContext } from './user-context';
 
 class App extends React.Component {
-  state = { theme: themes.light };
-  toggleTheme = () => {
-    const newTheme =
-      this.state.theme === themes.dark ? themes.light : themes.dark;
-    this.setState({ theme: newTheme });
-  };
+  state = { userData: userData };
   render() {
+    console.log(this.state.userData);
     return (
-      <div>
-        <ThemeContext.Provider value={this.state.theme}>
-          <ThemedButton onClick={this.toggleTheme}>Dynamic Theme</ThemedButton>
-        </ThemeContext.Provider>
-        <ThemedButton onClick={this.toggleTheme}>Default Theme</ThemedButton>
+      <div className="page">
+        <UserContext.Provider value={this.state.userData}>
+          <Header />
+        </UserContext.Provider>
       </div>
     );
   }
--- 01_react_context/task_hw_01/src/UserMenu.jsx
+++ 01_react_context/task_hw_01/src/UserMenu.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { ThemeContext } from './themes-context';
-
+import { UserContext } from './user-context';
 class UserMenu extends React.Component {
   render() {
-    // console.log(this.context);
     return (
-      <button
-        {...this.props}
-        style={{
-          backgroundColor: this.context.background,
-          color: this.context.fontColor,
-        }}
-        className="btn"
-      />
+      <div className="menu">
+        <span className="menu__greeting">Hello, {this.context.name}</span>
+        <img
+          alt="User Avatar"
+          src={this.context.avatar_url}
+          className="menu__avatar"
+        />
+      </div>
     );
   }
 }
 
-UserMenu.contextType = ThemeContext;
+UserMenu.contextType = UserContext;
 
 export default UserMenu;
