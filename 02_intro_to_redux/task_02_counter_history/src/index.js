7d04fb760fee68eaa53b51564e3661c185b28a9a
apache0505 <jiayulutsk@ukr.net>
2022-12-16 15:14:55 +0200

    sad

--- 02_intro_to_redux/task_02_counter_history/src/index.js
+++ 02_intro_to_redux/task_02_counter_history/src/index.js
@@ -21,7 +21,13 @@ resetBtn.addEventListener('click', onReset);
 store.subscribe(() => {
   const state = store.getState();
   const currentValue = state.history.reduce((acc, value) => acc + value, 0);
-  const historyString = state.history.join('');
+  // const historyString = state.history.join(' ');
+  const historyString = state.history.reduce(
+    (acc, val) =>
+      val > 0 ? `${acc}+${val.toString()}` : `${acc}${val.toString()}`,
+    ''
+  );
+  console.log(historyString);
   resultElem.textContent =
     historyString.length === 0 ? '' : `${historyString} = ${currentValue}`;
 });
