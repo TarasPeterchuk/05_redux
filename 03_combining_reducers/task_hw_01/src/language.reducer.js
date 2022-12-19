195b618341494becbb5302b0f960d388e9cded64
apache0505 <jiayulutsk@ukr.net>
2022-12-19 12:41:28 +0200

    asa

--- 03_combining_reducers/task_hw_01/src/language.reducer.js
+++ 03_combining_reducers/task_hw_01/src/language.reducer.js
@@ -1,15 +1,25 @@
 import { SET_LANGUAGE } from './language.actions';
-const initialState = {
-  language: 'en',
-};
+const initialState = 'en';
 const languageReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_LANGUAGE: {
-      return { ...state, language: action.payload.language };
+      return action.payload.language;
     }
     default:
       return state;
   }
 };
 
+// const initialState = {
+//   language: 'en',
+// };
+// const languageReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case SET_LANGUAGE: {
+//       return { ...state, language: action.payload.language };
+//     }
+//     default:
+//       return state;
+//   }
+// };
 export default languageReducer;
