92a879f8b899a80248c4c47667b2620e0de54a82
apache0505 <jiayulutsk@ukr.net>
2022-12-19 11:46:48 +0200

    sa

---
+++ 03_combining_reducers/task_02/src/index.js
@@ -0,0 +1,5 @@
+import { store } from './counter';
+import { increment, decrement } from './counter';
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(increment());
+store.dispatch(decrement());
