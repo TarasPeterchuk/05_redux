d101761c33043a743f58ac1db2cadcdeb0f51112
apache0505 <jiayulutsk@ukr.net>
2022-12-20 16:54:23 +0200

    as

---
+++ 06_middleware/task_01_loger_pt1/src/Counter.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as counterActions from './counter.actions';
+
+const Counter = ({ counter, increment, decrement, reset }) => {
+  return (
+    <div className="counter">
+      <button onClick={decrement} className="counter__button">
+        -
+      </button>
+      <span className="counter__value" onClick={reset}>
+        {counter}
+      </span>
+      <button onClick={increment} className="counter__button">
+        +
+      </button>
+    </div>
+  );
+};
+
+const mapState = (state) => {
+  return { counter: state };
+};
+
+const mapDispatch = {
+  increment: counterActions.increment,
+  decrement: counterActions.decrement,
+  reset: counterActions.reset,
+};
+
+const connector = connect(mapState, mapDispatch);
+export default connector(Counter);
