c17edd255fce8a10cec192a039a8c0ca31e05a5e
apache0505 <jiayulutsk@ukr.net>
2023-03-09 17:34:36 +0200

    as

--- 07_practice/task_01_todo_list/src/tasks/tasks.selectors.js
+++ 07_practice/task_01_todo_list/src/tasks/tasks.selectors.js
@@ -1,8 +1,6 @@
 import { createSelector } from 'reselect';
-export const tasksListSelector = (state) => state.tasks.tasksList;
-export const sortedTasksListSelector = createSelector(
-  [tasksListSelector],
-  (tasksList) => {
-    return tasksList.slice().sort((a, b) => a.done - b.done);
-  }
-);
+
+export const tasksListSelector = state => state.tasks.tasksList;
+export const sortedTasksListSelector = createSelector([tasksListSelector], tasksList => {
+  return tasksList.slice().sort((a, b) => a.done - b.done);
+});
